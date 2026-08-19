63a8c539c8b89dcaf0861ec02b5aa3938bf2ba94
Sanskar <sanskarin@outlook.in>
2026-08-19 09:59:58 +0530

    refactor: centralize application metadata

---
+++ src/config/app.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = 'DiceLab';
+export const APP_VERSION = '0.1.0';
+export const APP_CREDIT = 'Made by the Sanskar';
+
+export const PROJECT_URL = 'https://github.com/sanskarIN/dicelab';
+export const PRIVACY_URL = `${PROJECT_URL}/blob/main/PRIVACY.md`;
+export const RELEASES_URL = `${PROJECT_URL}/releases`;
+export const BMC_URL = 'https://buymeacoffee.com/sanskarIN';
+
+export const BUSINESS_EMAILS = ['sanskarin@outlook.in', 'sanskarin.business@gmail.com'] as const;
+export const SUPPORT_EMAIL = 'supportramsandesh@gmail.com';
