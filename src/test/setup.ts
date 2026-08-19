af4d259e21338b86340ff839201bde78aab2f720
Sanskar <sanskarin@outlook.in>
2026-08-19 09:50:11 +0530

    test: provide stable browser environment stubs

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,31 @@
 import '@testing-library/jest-dom/vitest';
+
+if (typeof window.matchMedia !== 'function') {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string): MediaQueryList => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => undefined,
+      removeListener: () => undefined,
+      addEventListener: () => undefined,
+      removeEventListener: () => undefined,
+      dispatchEvent: () => false,
+    }),
+  });
+}
+
+if (typeof URL.createObjectURL !== 'function') {
+  Object.defineProperty(URL, 'createObjectURL', {
+    writable: true,
+    value: () => 'blob:dicelab-test',
+  });
+}
+
+if (typeof URL.revokeObjectURL !== 'function') {
+  Object.defineProperty(URL, 'revokeObjectURL', {
+    writable: true,
+    value: () => undefined,
+  });
+}
