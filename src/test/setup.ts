3f7fc7cf906cb23d71fd59d6c955a32225145586
Sanskar <sanskarin@outlook.in>
2026-08-19 09:53:54 +0530

    test: stabilize animation frame behavior

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -16,6 +16,20 @@ if (typeof window.matchMedia !== 'function') {
   });
 }
 
+if (typeof window.requestAnimationFrame !== 'function') {
+  Object.defineProperty(window, 'requestAnimationFrame', {
+    writable: true,
+    value: (callback: FrameRequestCallback) => window.setTimeout(() => callback(performance.now()), 0),
+  });
+}
+
+if (typeof window.cancelAnimationFrame !== 'function') {
+  Object.defineProperty(window, 'cancelAnimationFrame', {
+    writable: true,
+    value: (handle: number) => window.clearTimeout(handle),
+  });
+}
+
 if (typeof URL.createObjectURL !== 'function') {
   Object.defineProperty(URL, 'createObjectURL', {
     writable: true,
